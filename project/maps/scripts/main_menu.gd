dbdae764005eeb33d9476f7e270845d1d5b51837
Jan Mesarč <jame_581@windowslive.com>
2024-12-12 00:19:02 +0100

    Add audio and timer to main menu, camera movement script
    Updated main_menu.tscn to include an AudioStreamPlayer and Timer node, with connections for button presses, audio completion, and timer timeouts. Updated main_menu.gd to reference these nodes and handle their events.
    
    Updated storyboard.tscn to include a new camera movement script and modified Camera2D properties. Created camera_movement.gd to handle camera rotation based on mouse movement.

--- project/maps/scripts/main_menu.gd
+++ project/maps/scripts/main_menu.gd
@@ -1,9 +1,17 @@
 extends Control
 
 @onready var version_label: Label = get_node("MarginContainer/HBoxContainer/LeftVBoxContainer/MainMenuVBox/VersionLabel")
+@onready var audio_stream_player: AudioStreamPlayer = get_node("AudioStreamPlayer")
+@onready var timer: Timer = get_node("Timer")
 
 func _ready() -> void:
 	version_label.text = Global.get_version()
 
 func _on_start_button_pressed() -> void:
 	Global.goto_scene("res://maps/storyboard.tscn")
+
+func _on_audio_stream_player_finished() -> void:
+	timer.start()
+
+func _on_timer_timeout() -> void:
+	audio_stream_player.play()
